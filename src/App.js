e31371f2a1034e27165b1121184d347d4f418473
Ryan Etedali <ryanetedali@gmail.com>
2022-08-02 18:46:17 -0500

    created Components Folder

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React, { useState } from 'react';
 
 function App() {
 
- const [entries, setEntries] = useState([])
+ const [entries, setEntries] = useState([{weight: 175, date:'1-23-23'}, {weight: 181, date:'2-23-23'}])
 
   return (
     <div>
-      <h3>hi baby</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Entry</th>
+            <th>Weight</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {entries.map((entry, index) => {
+            return (
+              <tr>
+                <td>{index+1}</td>
+                <td>{entry.weight}</td>
+                <td>{entry.date}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
