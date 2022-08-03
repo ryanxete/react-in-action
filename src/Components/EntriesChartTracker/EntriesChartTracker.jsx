301f321445aa1fad7f84e4c2e2f6840579f68394
Ryan Etedali <ryanetedali@gmail.com>
2022-08-03 18:49:47 -0500

    added google chart function

---
+++ src/Components/EntriesChartTracker/EntriesChartTracker.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Chart } from "react-google-charts";
+
+function EntriesChartTracker (props) {
+    return (
+        <Chart
+        chartType="ScatterChart"
+        data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+        width="100%"
+        height="400px"
+        legendToggle/>
+            );
+}
+
+export default EntriesChartTracker ;
